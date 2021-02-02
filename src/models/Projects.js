3895e5eb35245ad2fadea34ee40fe9b3f944acfb
Suraj Thapa <stsurajst@gmail.com>
2021-02-02 23:59:36 +0545

    model complete

--- src/models/Projects.js
+++ src/models/Projects.js
@@ -1,22 +1,32 @@
 import Sequelize from "sequelize";
 
 import { sequelize } from "../database/database";
+import Task from "./Tasks";
 
-sequelize.define("projects", {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
+const Project = sequelize.define(
+  "projects",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.TEXTdependency,
+    },
+    priority: {
+      type: Sequelize.INTEGER,
+    },
+    description: {
+      type: Sequelize.TEXT,
+    },
+    deliverydate: {
+      type: Sequelize.DATE,
+    },
   },
-  name: {
-    type: Sequelize.TEXTdependency,
-  },
-  priority: {
-    type: Sequelize.INTEGER,
-  },
-  description: {
-    type: Sequelize.TEXT,
-  },
-  deliverydate: {
-    type: Sequelize.DATE,
-  },
-});
+  {
+    timestamps: false,
+  }
+);
+Project.hasMany(Task, { foreignKey: "projectId", sourceKey: "id" });
+Task.belongsTo(Project, { foreignKey: "projectId", sourceKey: "id" });
+export default Project;
